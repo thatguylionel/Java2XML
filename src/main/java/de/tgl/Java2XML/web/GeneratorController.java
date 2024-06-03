3959d5ad2dae62e8cc14eca7a600cfbec3fe69b7
That Guy Lionel <12081026+thatguylionel@users.noreply.github.com>
2024-06-03 10:17:24 +0200

    Java / Jakarta 21 updates

--- src/main/java/de/tgl/Java2XML/web/GeneratorController.java
+++ src/main/java/de/tgl/Java2XML/web/GeneratorController.java
@@ -7,7 +7,6 @@ import de.tgl.Java2XML.util.FileReader;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 
-import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
@@ -15,7 +14,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 import java.util.logging.Level;
@@ -24,44 +22,38 @@ import java.util.logging.Logger;
 @Path("generateDocument")
 public class GeneratorController {
     private static final Logger LOGGER = Logger.getLogger(GeneratorController.class.getName());
-    private static final String RESOURCE_DIRECTORY = "generated";
-
 
     @GET
-    public static String generateDocument() throws Exception {
-        //  change directory
-        java.nio.file.Path directoryPath = Paths.get(RESOURCE_DIRECTORY);
-        Files.createDirectories(directoryPath);
+    public String generateDocument() throws Exception {
+        // Create the directory if it doesn't exist
+        java.nio.file.Path tempDirectory = Files.createTempDirectory("generated-");
 
-        String fileName = "Example-" + UUID.randomUUID() + ".xml";
-        directoryPath.resolve(fileName);
+        String fileName = UUID.randomUUID() + ".xml";
+        java.nio.file.Path filePath = tempDirectory.resolve(fileName);
 
         // First create Stax components we need
-        XMLInputFactory xmlInputFactory = XMLInputFactory.newFactory();
         XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newFactory();
-        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-        XMLStreamWriter sw = xmlOutputFactory.createXMLStreamWriter(fileOutputStream, StandardCharsets.UTF_8.name());
+        try (FileOutputStream fileOutputStream = new FileOutputStream(filePath.toFile())) {
+            XMLStreamWriter sw = xmlOutputFactory.createXMLStreamWriter(fileOutputStream, StandardCharsets.UTF_8.name());
+            XmlMapper mapper = new XmlMapper();
 
-        // then Jackson components
-        XmlMapper mapper = new XmlMapper(xmlInputFactory);
+            // Start document and setup content of document
+            sw.writeStartDocument();
+            setupRootAndContent(sw, mapper, filePath.toString());
+            sw.writeEndDocument();
 
-        // Start document and setup content of document
-        sw.writeStartDocument();
-        setupRootAndContent(sw, mapper, fileName);
-        sw.writeEndDocument();
+            // Flush and close writer/stream
+            sw.flush();
 
-        //  Flush and Close writer/stream
-        sw.flush();
-        sw.close();
-        fileOutputStream.close();
-        fileOutputStream.flush();
+            LOGGER.log(Level.INFO, "File Created - {0}", filePath);
 
-        LOGGER.log(Level.INFO, "File Created - " + fileName);
-
-        //  return contents of file to string
-        return FileReader.readFile(fileName, StandardCharsets.UTF_8);
+            // Return contents of file as a string
+            return FileReader.readFile(filePath.toString(), StandardCharsets.UTF_8);
+        } catch (XMLStreamException | IOException e) {
+            LOGGER.log(Level.SEVERE, "Error generating document", e);
+            throw e;
+        }
     }
-
 
     /**
      * The contents of the below method will generate this file example:
@@ -80,19 +72,13 @@ public class GeneratorController {
      * @throws XMLStreamException raises {@link XMLStreamException}
      * @throws IOException        raises {@link IOException}
      */
-    private static void setupRootAndContent(XMLStreamWriter sw, XmlMapper mapper, String name) throws XMLStreamException, IOException {
+    private void setupRootAndContent(XMLStreamWriter sw, XmlMapper mapper, String name) throws XMLStreamException, IOException {
         sw.writeStartElement("root");
         sw.writeStartElement("details");
-
-        // Write whatever content POJOs...
         mapper.writeValue(sw, new Simple(1, 2));
         mapper.writeValue(sw, new Employee("CAJ", "Cathy Jones", 300000, List.of("Max", "Minnie")));
-        sw.writeEndElement();   //  close DETAILS element
-
+        sw.writeEndElement();
         sw.writeComment("The file has generated at location " + name);
-        sw.writeEndElement();   //  close ROOT element
+        sw.writeEndElement();
     }
-
-
 }
-
